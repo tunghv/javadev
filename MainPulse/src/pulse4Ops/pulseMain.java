385ffa524b546311a3e58db76671710134bad817
tunghv <huynhvantung@gmail.com>
2021-09-03 02:44:42 +0700

    remove tail os iss01

--- MainPulse/src/pulse4Ops/pulseMain.java
+++ MainPulse/src/pulse4Ops/pulseMain.java
@@ -4,7 +4,7 @@ public class pulseMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World, Java!@!@ huhuhu haha");
+		System.out.println("Hello World!");
 	}
 
 }

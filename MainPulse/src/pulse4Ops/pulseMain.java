4fa88c0ef8e249d710298eb12a5d9b27614ca3cf
tunghv <huynhvantung@gmail.com>
2021-09-03 08:50:19 +0700

    final hotfix 00

--- MainPulse/src/pulse4Ops/pulseMain.java
+++ MainPulse/src/pulse4Ops/pulseMain.java
@@ -4,6 +4,6 @@ public class pulseMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Java 9 SDK, Hello World!");
+		System.out.println("life is a journey");
 	}
 }

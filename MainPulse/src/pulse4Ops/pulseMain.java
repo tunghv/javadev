ea123b95097c020290d0399f61e9279badb59c4f
tunghv <huynhvantung@gmail.com>
2021-09-03 08:28:26 +0700

    release version

--- MainPulse/src/pulse4Ops/pulseMain.java
+++ MainPulse/src/pulse4Ops/pulseMain.java
@@ -4,11 +4,6 @@ public class pulseMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//<<<<<<< HEAD
-		System.out.println("Hello World!Hot Fix 02");
-//=======
-		System.out.println("Hello World! hot fix");
-//>>>>>>> hotfix
+		System.out.println("Java, Hello World!");
 	}
-
 }
